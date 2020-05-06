4ef0dabbd37884ab824cddfff272de105cb3a766
stephen pesce <stevepesce879@gmail.com>
2020-05-06 20:12:52 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,24 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+ pigeon_data = {}
+ data.each{|(outer_key, outer_val)|
+   outer_val.each{|mid_key, mid_val|
+     mid_val.each{|name|
+     
+      puts "#{outer_key}"
+     if pigeon_data[name]
+       puts "if ran"
+        pigeon_data[name][outer_key] << mid_val.to_s 
+     else
+       puts "else ran"
+       pigeon_data[name] = {outer_key: []}
+     end
+     }
+     pp pigeon_data
+    
+   }
+ }
+ binding.pry
+ 
 end
